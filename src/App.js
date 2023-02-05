1a07ddd1dfe8cfe55862e41f9c86689829ea0fe0
ShruthiBandi-27 <shruthi27998@gmail.com>
2023-02-05 17:15:09 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,40 @@
+// import logo from './logo.svg';
+// import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
+import React from 'react'
+import Card from './Card'
+import './style.css'
+
+export default function App() {
+  return (
+    <div>
+      <Card titles={["FREE","PLUS","PRO"]} headings={["$0","$9","$49"]}/>
+    </div>
+  )
+}
+
+
+
+
---
+++ src/utils.js
@@ -0,0 +1,33 @@
+export let data = [
+    {
+        d1: "Single User",
+        d2: "5GB Storage",
+        d3: "Unlimited Public Projects",
+        d4: "Community Access",
+        d5: "Unlimited Private Projects",
+        d6: "Dedicated Phone Support",
+        d7: "Free Subdomain",
+        d8: "Monthly Status Reports"
+    },
+    {
+        d1: "5 Users",
+        d2: "50GB Storage",
+        d3: "Unlimited Public Projects",
+        d4: "Community Access",
+        d5: "Unlimited Private Projects",
+        d6: "Dedicated Phone Support",
+        d7: "Free Subdomain",
+        d8: "Monthly Status Reports"
+    },
+    {
+        d1: "Unlimited Users",
+        d2: "150GB Storage",
+        d3: "Unlimited Public Projects",
+        d4: "Community Access",
+        d5: "Unlimited Private Projects",
+        d6: "Dedicated Phone Support",
+        d7: "Unlimited Free Subdomain",
+        d8: "Monthly Status Reports"
+    },
+    
+]
